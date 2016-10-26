c1bf1d803d7a4524b34990d013ca3583a25888bb
Wozthott <awizardnamedwoz@gmail.com>
2016-10-26 14:22:20 -0500

    Files

---
+++ Source_CCA-Prot.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+#define p getch();
+#define e << endl;
+#define o cout <<
+#define s ;
+
+int main () {
+
+o"Accessing Network..."e
+p
+o"!Access Denied!"e
+p
+o"In"s p o"t"s p o"il"s p o"i"s p o"ze"s p o" c"s p o"md"s p o" Pro"s p o"to"s p o"col"e
+p
+o"dir"s p o" cd C"s p o"rack"s p o" CNG`"s p o"1.B"s p o"AT"e p
+for (int i = 0; i < 5; i++) {
+    o"A"s p o"pp"s p o"lyin"s p o"g Re"s p o"ques"s p o"t Se"s p o"tting"s p
+    o" To (N"s p o"UL"s o"L) "e p
+    o"Ad"s p o"m"s p o"in "s p o"Agg"s p o"ro"s p o" Ta"s p o"bles"s p o" Rero"s p o"uted"e p
+    o"Ru"s p o"nn"s p o"ing"s p o" Cp"s p o"t."s p o"Cru"s p o"nch's"s p o" Algo"s p o"rit"s p o"hem"e
+    p
+    o"whi"s p o"le "s p o"(tr"s p o"ue"s p o")"s p o" if"s p o" ("s p o"ca"s p o"u"s p o"ght"s p o") ex"s p o"ecu"s p
+    o"te "s p o"ru"s p o"n"s p o"; "e p o"el"s p o"se "s p o"co"s p o"nti"s p o"nu"s o"e;"e p
+    o"co"s p o"n"s p o"ti"s p o"nu"s p o"e"s p o" ="s p o"="s p o" tr"s p o"ue;"e p
+    o"vo"s p o"i"s p o"d N"s p o"et"s p o"w"s p o"or"s p o"k"s p o"::a"s p o"cc"s p o"e"s p o"s"s p o"s(i"s p o"nt "s p
+    o"si"s p o"ze)"s p o"{"e p
+    o" fo"s p o"r "s p o"("s p o"in"s p o"t i"s p o" ="s p o" 0;"s p o" i"s p o" <"s p o" s"s p
+    o"iz"s p o"e;"s p o" i"s p o"+"s p o"+)"e p
+    o"     "s p o"Us"s p o"ers"s p o"[i]"s p o" = "s p o"i +"s p o" 1;"e p
+    o" p"s p o"ing "s p o"= "s p o"-1"s p o";"e p
+o"}"e p
+p
+if (i == 2) {
+      break;
+}
+o"!Access Denied!"e
+p
+
+}
+p
+for (int i = 0; i < 10; i++) {
+o"*Success*"e 
+o"..."e 
+}
+o"All Arguments Returned Non-Zero"s 
+o"..."e
+o"Network Access Granted"e 
+o"Elavated Access found"e 
+o"Accout User: Admin"e
+o"Logged-In"e 
+o"PapersPlease.exe Up-Loaded To All In-Coming Communications"e
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+p
+/*
+o"All"s p o" a"s p o"rg"s p o"um"s p o"en"s p o"ts "s p o"ret"s p o"ur"s p o"ne"s p o"d "s p
+o"no"s p o"n-"s p o"ze"s p o"ro"s p o"."e p o" N"s p o"et"s p o"wo"s p o"rk"s p o" Ac"s p o"es"s p
+o"s "s p o"Gr"s p o"an"s p o"te"s p o"d"e
+*/
+}
